362392afd5be660502f18a1bb1e2515c658c89ef
ShogoFukuda <42861916+Sho5dayo@users.noreply.github.com>
2021-10-08 22:08:04 +0900

    Add files via upload
    試合結果の登録ができる機能を実装

---
+++ PHP課題02_福田/funcs.php
@@ -0,0 +1,32 @@
+<?php
+function dbConnect()
+{
+
+    $dsn = 'mysql:host=localhost;dbname=scoring_machine;charset=utf8';
+    $user = 'administrator';
+    $pass = 'P@ssw0rd';
+
+    try {
+        $dbh = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);
+    } catch (PDOException $e) {
+        echo '接続失敗' . $e->getMessage();
+        exit();
+    };
+    return $dbh;
+}
+
+//SQLエラー関数：sql_error($stmt)
+function sql_error($stmt)
+{
+    $error = $stmt->errorInfo();
+    exit('SQLError:' . print_r($error, true));
+}
+
+//リダイレクト関数: redirect($file_name)
+function redirect($file_name)
+{
+    header('Location: ' . $file_name);
+    exit();
+}
